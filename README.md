ef2a05e9b590522f553dc0eaca5183bb16ebfdea
Rainier Ababao <rainier.ababao@gmail.com>
2015-11-14 09:39:03 -0500

    Add WebRTC stream capture

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # princeton
-HackPrinceton 2015 hack
+## HackPrinceton 2015 hack
+
+Rainier Ababao.
+
+Uses Flask and WebRTC on the backend.
+
+APIs
+- Microsoft Project Oxford  
+- Indico  
+- Clarifai  
+- gensim (if time)  
+- linode? (if time)  
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import logging
+from models.camera import Camera
 from functools import wraps
 from flask import Flask, render_template, request, redirect, session, Response
 
@@ -17,43 +18,50 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 
-@app.before_first_request
-def setup_auth_tokens():
-    session['authenticated_1'] = False
-    session['authenticated_2'] = False
-
-
-def onefa_authorized(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if not session['authenticated_1']:
-            return Response('You need to be authorized to view this page.',
-                            401, {'WWW-Authenticate': 'Basic realm="Needs authorization."'})
-        return f(*args, **kwargs)
-    return wrapper
-
-
-def twofa_authorized(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if not session['authenticated_1'] or not session['authenticated_2']:
-            return Response('You need to be authorized to view this page.',
-                            401, {'WWW-Authenticate': 'Basic realm="Needs authorization."'})
-        return f(*args, **kwargs)
-    return wrapper
-
-
 @app.route('/')
 def index():
-    if os.environ.get('APP_CONFIG', None) == 'TestingConfig':
-        session['authenticated_1'] = True
-        session['authenticated_2'] = True
-    if ('authenticated_1' not in session or 'authenticated_2' not in session
-        or not session['authenticated_1'] or not session['authenticated_2']):
-        session['authenticated_1'] = False
-        session['authenticated_2'] = False
-        session['random_key'] = '000000'
-        return render_template('index.html')
+    return render_template('index.html')
+
+
+# I don't need two-factor... yet
+
+# @app.before_first_request
+# def setup_auth_tokens():
+#     session['authenticated_1'] = False
+#     session['authenticated_2'] = False
+#
+#
+# def onefa_authorized(f):
+#     @wraps(f)
+#     def wrapper(*args, **kwargs):
+#         if not session['authenticated_1']:
+#             return Response('You need to be authorized to view this page.',
+#                             401, {'WWW-Authenticate': 'Basic realm="Needs authorization."'})
+#         return f(*args, **kwargs)
+#     return wrapper
+#
+#
+# def twofa_authorized(f):
+#     @wraps(f)
+#     def wrapper(*args, **kwargs):
+#         if not session['authenticated_1'] or not session['authenticated_2']:
+#             return Response('You need to be authorized to view this page.',
+#                             401, {'WWW-Authenticate': 'Basic realm="Needs authorization."'})
+#         return f(*args, **kwargs)
+#     return wrapper
+#
+#
+# @app.route('/')
+# def index():
+#     if os.environ.get('APP_CONFIG', None) == 'TestingConfig':
+#         session['authenticated_1'] = True
+#         session['authenticated_2'] = True
+#     if ('authenticated_1' not in session or 'authenticated_2' not in session
+#         or not session['authenticated_1'] or not session['authenticated_2']):
+#         session['authenticated_1'] = False
+#         session['authenticated_2'] = False
+#         session['random_key'] = '000000'
+#         return render_template('index.html')
 
 
 if __name__ == '__main__':
